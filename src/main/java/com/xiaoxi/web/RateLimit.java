0ee0b0cb9c91e5fce2f072217c19c76487637713
hongxiyang <858776278@qq.com>
2016-06-25 20:25:28 +0800

    对restful接口进行一些更改

--- src/main/java/com/xiaoxi/web/RateLimit.java
+++ src/main/java/com/xiaoxi/web/RateLimit.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,9 +26,10 @@ public class RateLimit {
 
     @RequestMapping(value = "/", method = RequestMethod.POST,
                     produces = {"application/json;charset=utf-8"})
+    @ResponseBody
     public boolean limit(@RequestParam(value = "action")String action,
                          @RequestParam(value = "feature")String feature,
-                         @RequestParam(value = "timestamp")long timestamp,
+                         @RequestParam(value = "timestamp")String timestamp,
                          @RequestParam(value = "rate")String rate,
                          HttpServletResponse httpServletResponse) {
 
@@ -43,7 +45,7 @@ public class RateLimit {
                 httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "请核查 rate 参数设置");
             }
             int number = Integer.parseInt(rateSplit[0]);
-            limit = rateHandle.isLimit(action, feature, timestamp, number, rat);
+            limit = rateHandle.isLimit(action, feature, Long.parseLong(timestamp), number, rat);
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
             throw new RuntimeException(e);
--- src/test/java/com/xiaoxi/web/testRateLimit.java
+++ src/test/java/com/xiaoxi/web/testRateLimit.java
@@ -1,12 +1,24 @@
 package com.xiaoxi.web;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import sun.net.www.http.HttpClient;
 
+import javax.xml.ws.http.HTTPException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by YanYang on 2016/6/25.
@@ -17,6 +29,32 @@ import java.net.URL;
 public class testRateLimit {
     @Test
     public void testRateLimit() throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
 
+        HttpPost httpPost = new HttpPost("http://localhost:8080/limit/");
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        params.add(new BasicNameValuePair("action", "POST"));
+        params.add(new BasicNameValuePair("feature", "xiaoxi"));
+        long timestamp = System.currentTimeMillis();
+        params.add(new BasicNameValuePair("timestamp",String.valueOf(timestamp)));
+        params.add(new BasicNameValuePair("rate", "5/s"));
+
+        UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(params, "utf-8");
+
+        httpPost.setEntity(urlEncodedFormEntity);
+
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+
+        HttpEntity httpEntity = httpResponse.getEntity();
+
+        String result = EntityUtils.toString(httpEntity, "utf-8");
+
+        System.out.println("结果" + result);
+
+        httpResponse.close();
+
+        httpClient.close();
     }
 }
